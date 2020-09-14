b82612e169d572cd067e2de248374be76ada028a
MrWifeRespecter <70580263+MrWifeRespecter@users.noreply.github.com>
2020-09-14 19:41:55 +0200

    FUCK YOU
    JAG VANN
    ÄT RÖV

---
+++ U5-18/U5-18/U5_18.py
@@ -0,0 +1,5 @@
+for i in range(0,10):
+    for j in range(1,11):
+        print(i, end=" ")
+        if j == 10:
+            print("\n")
---
+++ U5-19/U5-19/U5_19.py
@@ -0,0 +1,30 @@
+#temp=10
+#blank=""
+#for i in range(1,11):
+#    for j in range(0,temp):
+#        print(blank, j, end=" ")
+#        for k in range(0,j):
+#            blank+=" "  
+#    print("\n")
+#    temp=10-i
+
+
+
+
+
+
+
+
+
+
+temp=10
+blanksteg=" "
+for x in range(1,11):
+        print(blanksteg, end=" ")
+        blanksteg+="  "
+        for j in range(0,temp):
+            print(j, end=" ")
+            temp=10-x
+        print("\n")
+
+    
