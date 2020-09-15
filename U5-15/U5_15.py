cfbfcab930cf4aefdbf9a55043ac99819bdf39e1
MrWifeRespecter <viggo034@gmail.com>
2020-09-15 08:41:51 +0200

    inget viktigt

--- U5-15/U5_15.py
+++ U5-15/U5_15.py
@@ -1,2 +1,5 @@
-for i in range(1,10):
-    print(i, end=" ")
+for x in range(1,6):
+    for i in range(1,10):
+        print(i, end=" ")
+        if i == 9:
+            print("\n")
--- lektion 3/lektion_3.py
+++ lektion 3/lektion_3.py
@@ -4,7 +4,7 @@ print("Här {} skriver vi lite text" .format("xxx"))
 #exempel: här använder vi {:.3f} för att visa att vi vill att kosen ska skriva ut svaret som användaren ger som en float med 3 decimaler.
 pris=float(input("skriv in pris "))
 print("varan kostar {:.3f} " .format(pris))
-print("varann kostar {0} kr {1}, {2} {3}".format(pris, "slut", "andra", "tredje"))
+print("varan kostar {0} kr {1}, {2} {3}".format(pris, "slut", "andra", "tredje", "fjärde"))
 # {:.4s} ger en sträng med upptagande av mist 4 teckenpositioner
 pris2=str(input("test "))
 print("{:.4s} test".format(pris2))
